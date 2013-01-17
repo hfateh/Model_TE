leg.py now has correct J formula, includes area, and works fine with good plot of R_load vs P

Instances/te_inst.py
@@ -23,75 +23,42 @@ te_pair.T_h_conv = 800.
 te_pair.U_cold = 800000.
 te_pair.U_hot = 20000.
 
-te_pair.R_load = 0.0035
-te_pair.solve_te_pair()
-print "\nPower is ", te_pair.P
+#te_pair.solve_te_pair()
 
-te_pair.R_load = 0.0035
-te_pair.solve_te_pair()
-print "\nPower is ", te_pair.P
+R_load = np.linspace(0.0001, 0.0005, 2)
+R_internal = np.zeros(R_load.size)
+P = np.zeros(R_load.size)
+for i in range(R_load.size):
+    te_pair.R_load = R_load[i]
+    te_pair.solve_te_pair()
+    print "Actual J is ", te_pair.J
+    P[i] = te_pair.P
+    print "Power is ", P[i]
+    R_internal[i] = te_pair.R_internal
 
-# te_pair.R_load = 0.0035
-# te_pair.solve_te_pair()
-# print "\nPower is ", te_pair.P
+# Plot configuration
+FONTSIZE = 14
+plt.rcParams['axes.labelsize'] = FONTSIZE
+plt.rcParams['axes.titlesize'] = FONTSIZE
+plt.rcParams['legend.fontsize'] = FONTSIZE
+plt.rcParams['xtick.labelsize'] = FONTSIZE
+plt.rcParams['ytick.labelsize'] = FONTSIZE
+plt.rcParams['lines.linewidth'] = 1.5
+plt.rcParams['lines.markersize'] = 10
 
-# te_pair.R_load = 0.0035
-# te_pair.solve_te_pair()
-# print "\nPower is ", te_pair.P
+plt.close()
 
+plt.figure()
 
+plt.plot(R_load, P, 's')
+# plt.plot(R_load, R_internal, 'o')
+plt.grid()
+plt.xlabel('R_load (ohms)')
+plt.ylabel('Power (W)')
+#plt.ylim(0,0)
+#plt.xlim(0,0)
 
-
-
-
-
-
-# print "Ntype V is ", te_pair.Ntype.V
-# print "Ntype J is ", te_pair.Ntype.J
-# print "Ptype V is ", te_pair.Ptype.V
-# print "Ptype J is ", te_pair.Ptype.J
-
-
-
-
-
-
-
-
-
-
-# R_load = np.linspace(0.0001, 0.0005, 5)
-# R_internal = np.zeros(R_load.size)
-# P = np.zeros(R_load.size)
-# for i in range(R_load.size):
-#     te_pair.R_load = R_load[i]
-#     te_pair.solve_te_pair()
-#     P[i] = te_pair.P
-#     R_internal[i] = te_pair.R_internal
-
-# # Plot configuration
-# FONTSIZE = 14
-# plt.rcParams['axes.labelsize'] = FONTSIZE
-# plt.rcParams['axes.titlesize'] = FONTSIZE
-# plt.rcParams['legend.fontsize'] = FONTSIZE
-# plt.rcParams['xtick.labelsize'] = FONTSIZE
-# plt.rcParams['ytick.labelsize'] = FONTSIZE
-# plt.rcParams['lines.linewidth'] = 1.5
-# plt.rcParams['lines.markersize'] = 10
-
-# plt.close()
-
-# plt.figure()
-
-# plt.plot(R_load, P, 's')
-# # plt.plot(R_load, R_internal, 'o')
-# plt.grid()
-# plt.xlabel('R_load (ohms)')
-# plt.ylabel('Power (W)')
-# #plt.ylim(0,0)
-# #plt.xlim(0,0)
-
-# plt.show()
+#plt.show()
 
 
 

Modules/leg.py
@@ -49,10 +49,6 @@ class Leg(object):
             types.MethodType(mat_prop.set_TEproperties, self)
             )
 
-    def set_constants(self):
-
-        self.x = np.linspace(0., self.length, self.nodes)
-
     def set_ZT(self):
 
         self.ZT = (
@@ -61,7 +57,14 @@ class Leg(object):
 
     def set_J(self):
 
-        self.J = self.Vs / (self.R_load + self.R_internal)
+        self.J = (
+            (self.Vs) / ((self.area) * (self.R_load +
+            self.R_internal))
+            )
+
+    def set_constants(self):
+
+        self.x = np.linspace(0., self.length, self.nodes)
 
     def solve_leg(self):
         
@@ -193,7 +196,8 @@ class Leg(object):
 
         # this is what J needs to be equal to 
         self.J_correct = (
-            self.Vs / (self.R_load + self.R_internal)
+            (self.Vs) / ((self.area) * (self.R_load +
+            self.R_internal))
             )
 
         # the difference between the correct J and calculated J