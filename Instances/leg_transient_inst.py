I sign changed in te_pair.py so it calculates correct q_x for N and P type, made other changes too

Instances/leg_transient_inst.py
@@ -15,7 +15,7 @@ leg = leg.Leg()
 leg.length = 1.e-3
 # leg.I = 13.
 leg.material = 'HMS'
-leg.nodes = 50
+leg.nodes = 10
 leg.t_array = np.linspace(0, 1, 10)
 
 leg.T_h_conv = 400.

Modules/leg.py
@@ -26,7 +26,7 @@ class Leg(object):
         self.Vs = 1.64/256.0
         self.R_internal = 1.0/256
 
-        self.material = 'HMS'
+        self.material = 'MgSi'
         self.T_h_conv = 443.
         self.T_c_conv = 323.
         self.U_hot = 54.e2
@@ -100,6 +100,9 @@ class Leg(object):
         self.V = self.R_load * self.I
         self.R_internal = self.R_x[-1]
 
+        # print "\nI is ", self.I
+        # print "\nq_x is found to be \n", self.q_x
+
         # Multiply q_h and q_c by area to get rid of this error
 
         # self.P = self.R_load * self.I ** 2
@@ -140,6 +143,9 @@ class Leg(object):
         dT_dx = (
             (J * self.alpha * T - q) / self.k
             )
+        
+        # print "\nJ is ", J
+        # print "\nalpha is ", self.alpha
         dq_dx = (
             (self.rho * J ** 2. * (1+self.ZT)) - (J * self.alpha * q /
             self.k)
@@ -161,7 +167,7 @@ class Leg(object):
 
         self.q_c = - (
             self.alpha * self.T_c * J - delta_T / self.length *
-        self.k - J ** 2 * self.length * self.rho
+            self.k - J ** 2 * self.length * self.rho
             )
 
         self.q_h = - (
@@ -292,6 +298,8 @@ class Leg(object):
         self.return_array = (
             np.array([dT_dt, dq_dt, dVs_dt, dR_dt]).flatten()
             )
+
+        print "\nreturn_array is \n", self.return_array
         # print "\nreturn array is \n", self.return_array
         return self.return_array
 

Modules/te_pair.py
@@ -13,7 +13,6 @@ class TE_Pair(object):
         """ """
         self.R_load_total = 1.0
         self.pairs = 128.0
-        # self.R_load = 1.0/128.0
         self.length = 1.e-3
         self.leg_area_ratio = 0.8
         self.fill_fraction = 0.3
@@ -28,7 +27,11 @@ class TE_Pair(object):
         self.Ntype.material = 'MgSi'
 
         self.nodes = 10
-        # self.set_constants() was initially here
+        self.t_array = np.linspace(0., 2., 10)
+        self.T_h_conv = 443.
+        self.T_c_conv = 343.
+        self.U_hot = 54.e3
+        self.U_cold = 253.e3
 
     def set_R_load(self):
         """ """
@@ -42,7 +45,6 @@ class TE_Pair(object):
         self.I = (
             self.Vs/(self.R_load + self.R_internal)
             )
-        # self.set_leg_areas() #was initially here
         self.Ntype.I = -self.I
         self.Ptype.I = self.I
 
@@ -73,19 +75,22 @@ class TE_Pair(object):
         self.Ptype.Vs = self.Vs
         self.Ntype.R_internal = self.R_internal
         self.Ptype.R_internal = self.R_internal
+        self.Ptype.t_array = self.t_array
+        self.Ntype.t_array = self.t_array
+        # self.Ptype.U_hot = self.U_hot
+        # self.Ntype.U_hot = self.U_hot
+        # self.Ptype.U_cold = self.U_cold
+        # self.Ntype.U_cold = self.U_cold
+        # self.Ptype.T_h_conv = self.T_h_conv
+        # self.Ntype.T_h_conv = self.T_h_conv
+        # self.Ptype.T_c_conv = self.T_c_conv
+        # self.Ntype.T_c_conv = self.T_c_conv
 
     def set_q_guess(self):
 
         """ """
         self.Ntype.set_q_guess()
         self.Ptype.set_q_guess()
-
-    # I dont know why this is here, its unnecessary, delete this
-    # def set_TEproperties(self, T_props):
-
-    #     """ """
-    #     self.Ntype.set_TEproperties(T_props)
-    #     self.Ptype.set_TEproperties(T_props)
 
     def solve_te_pair_once(self):
 
@@ -118,6 +123,10 @@ class TE_Pair(object):
         self.T_h = knob_arr[2]
         self.I = knob_arr[3]
 
+        # Following two lines are very important
+        self.Ptype.I = self.I
+        self.Ntype.I = -self.I
+
         self.Ptype.T_h = self.T_h
         self.Ntype.T_h = self.T_h
 
@@ -125,7 +134,6 @@ class TE_Pair(object):
 
         self.q_c_conv = self.U_cold * (self.T_c - self.T_c_conv)
         self.q_h_conv = self.U_hot * (self.T_h_conv - self.T_h)
-        #print "I in this run is ", self.I
         
         self.I_correct = (
             self.Vs / (self.R_load + self.R_internal)
@@ -135,13 +143,6 @@ class TE_Pair(object):
         q_c_error = self.q_c - self.q_c_conv
         q_h_error = self.q_h - self.q_h_conv
         I_error = self.I_correct - self.I
-        # print "\n"
-        # print "\n"
-        # print "\nI_correct is ", self.I_correct
-        # print "I_guess is ", self.I
-        # print "I_error is ", I_error
-        # print "\n"
-        # print "\n"
 
         self.error = (
             np.array([T_c_error, q_c_error, q_h_error, I_error]).flatten()
@@ -156,7 +157,6 @@ class TE_Pair(object):
         self.Ntype.T_h = self.T_h_conv
         self.Ptype.T_c = self.T_c_conv
         self.Ntype.T_c = self.T_c_conv
-        # self.set_I()  # was initially here
         self.set_q_guess()
 
         knob_arr0 = (
@@ -164,11 +164,304 @@ class TE_Pair(object):
         self.T_h_conv, self.I])
             )
 
-        # self.Ptype.T_c_goal = None
-        # self.Ntype.T_c_goal = None
-
         self.fsolve_output = fsolve(self.get_error, x0=knob_arr0)
         self.V = self.I * self.R_load
         self.P = self.I * self.V
         self.P_total = self.P * self.pairs
+
+    
+
+
+
+
+
+
+
+    def solve_te_pair_transient_once(self):
+        """ """
+        self.solve_te_pair()
+        # Ntype temperature distribution is correct
+        self.Ntype.solve_leg_transient_once()
+        # Ptype distribution is showing wrong, why???
+        self.Ptype.solve_leg_transient_once()
+        
+
+# Bunch of errors which I need to organize and find a way to equate
+# with respect to correct values
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# there is a working te_pair at the bottom
+# scroll down
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import numpy as np
+# from scipy.optimize import fsolve
+
+# import leg
+# reload(leg)
+
+# class TE_Pair(object):
+#     """ TE_pair
+#     """
+
+#     def __init__(self):
+
+#         """ """
+#         self.R_load_total = 1.0
+#         self.pairs = 128.0
+#         # self.R_load = 1.0/128.0
+#         self.length = 1.e-3
+#         self.leg_area_ratio = 0.8
+#         self.fill_fraction = 0.3
+#         self.Vs = 1.64/256. 
+#         self.R_internal = 1./256.
+#         self.Vs = 1.64/256.
+        
+#         self.Ptype = leg.Leg()
+#         self.Ntype = leg.Leg()
+
+#         self.Ptype.material = 'HMS'
+#         self.Ntype.material = 'MgSi'
+
+#         self.nodes = 10
+#         # self.set_constants() was initially here
+
+#     def set_R_load(self):
+#         """ """
+#         self.R_load = (
+#             self.R_load_total / self.pairs
+#             )
+
+#     def set_I(self):
+
+#         """ """
+#         self.I = (
+#             self.Vs/(self.R_load + self.R_internal)
+#             )
+#         # self.set_leg_areas() #was initially here
+#         self.Ntype.I = -self.I
+#         self.Ptype.I = self.I
+
+#     def set_leg_areas(self):
+#         """ """
+#         self.Ntype.area = self.Ptype.area * self.leg_area_ratio
+#         self.area_void = (
+#             ((1.- self.fill_fraction) * (self.Ntype.area +
+#         self.Ptype.area)) / (self.fill_fraction)
+#             )
+#         self.area = (
+#             self.Ntype.area + self.Ptype.area + self.area_void
+#             )
+
+#     def set_constants(self):
+
+#         """ """
+#         self.set_R_load()
+#         self.set_leg_areas()
+#         self.set_I()
+#         self.Ntype.length = self.length
+#         self.Ptype.length = self.length
+#         self.Ptype.nodes = self.nodes
+#         self.Ntype.nodes = self.nodes
+#         self.Ntype.set_constants()
+#         self.Ptype.set_constants()
+#         self.Ntype.Vs = -self.Vs
+#         self.Ptype.Vs = self.Vs
+#         self.Ntype.R_internal = self.R_internal
+#         self.Ptype.R_internal = self.R_internal
+
+#     def set_q_guess(self):
+
+#         """ """
+#         self.Ntype.set_q_guess()
+#         self.Ptype.set_q_guess()
+
+#     # I dont know why this is here, its unnecessary, delete this
+#     # def set_TEproperties(self, T_props):
+
+#     #     """ """
+#     #     self.Ntype.set_TEproperties(T_props)
+#     #     self.Ptype.set_TEproperties(T_props)
+
+#     def solve_te_pair_once(self):
+
+#         """ """
+#         self.Ntype.solve_leg_once(self.Ntype.q_h)
+#         self.Ptype.solve_leg_once(self.Ptype.q_h)
+#         self.T_c = self.Ntype.T_c
+
+#         self.q_h = (
+#             (self.Ptype.q_h * self.Ptype.area + self.Ntype.q_h *
+#              self.Ntype.area) / self.area * 0.001
+#             )
+
+#         self.q_c = (
+#             (self.Ptype.q_c * self.Ptype.area + self.Ntype.q_c *
+#              self.Ntype.area) / self.area * 0.001
+#             )
+
+#         self.Vs = -self.Ntype.Vs + self.Ptype.Vs
+
+#         self.R_internal = ( 
+#             self.Ntype.R_internal + self.Ptype.R_internal
+#             )
+
+#     def get_error(self, knob_arr):
+
+#         """ """
+#         self.Ntype.q_h = knob_arr[0]
+#         self.Ptype.q_h = knob_arr[1]
+#         self.T_h = knob_arr[2]
+#         self.I = knob_arr[3]
+
+#         self.Ptype.T_h = self.T_h
+#         self.Ntype.T_h = self.T_h
+
+#         self.solve_te_pair_once()
+
+#         self.q_c_conv = self.U_cold * (self.T_c - self.T_c_conv)
+#         self.q_h_conv = self.U_hot * (self.T_h_conv - self.T_h)
+#         #print "I in this run is ", self.I
+        
+#         self.I_correct = (
+#             self.Vs / (self.R_load + self.R_internal)
+#             )
+
+#         T_c_error = self.Ntype.T_c - self.Ptype.T_c
+#         q_c_error = self.q_c - self.q_c_conv
+#         q_h_error = self.q_h - self.q_h_conv
+#         I_error = self.I_correct - self.I
+#         # print "\n"
+#         # print "\n"
+#         # print "\nI_correct is ", self.I_correct
+#         # print "I_guess is ", self.I
+#         # print "I_error is ", I_error
+#         # print "\n"
+#         # print "\n"
+
+#         self.error = (
+#             np.array([T_c_error, q_c_error, q_h_error, I_error]).flatten()
+#             )
+#         return self.error
+
+#     def solve_te_pair(self):
+
+#         """ """
+#         self.set_constants()
+#         self.Ptype.T_h = self.T_h_conv 
+#         self.Ntype.T_h = self.T_h_conv
+#         self.Ptype.T_c = self.T_c_conv
+#         self.Ntype.T_c = self.T_c_conv
+#         # self.set_I()  # was initially here
+#         self.set_q_guess()
+
+#         knob_arr0 = (
+#             np.array([self.Ntype.q_h_guess, self.Ptype.q_h_guess,
+#         self.T_h_conv, self.I])
+#             )
+
+#         # self.Ptype.T_c_goal = None
+#         # self.Ntype.T_c_goal = None
+
+#         self.fsolve_output = fsolve(self.get_error, x0=knob_arr0)
+#         self.V = self.I * self.R_load
+#         self.P = self.I * self.V
+#         self.P_total = self.P * self.pairs
+
+
+
+
 