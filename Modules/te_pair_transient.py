5e28275e79f6d4b8ef4ce288d8ce8970c70fdf9c
Haiyan Fateh <hfateh@utexas.edu>
2013-02-04 08:32:04 -0600

    te_transient_inst.py added in Instances

---
+++ Modules/te_pair_transient.py
@@ -0,0 +1,7 @@
+# First run te_pair steady state, most of the iterations and boundary
+# conditions can be easily obtained from there. 
+
+# What do I get from te_pair steady state ???
+# T_x, q_x, Vs_x, and R_x for each leg.
+
+# 
